d42ea5a4d7010fe9399a1624d14970cad83236c9
sylwestergiers <44543218+sylwestergiers@users.noreply.github.com>
2022-10-16 15:05:11 +0200

    Add messages and params to  top menu

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -41,6 +41,8 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
             <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
         </div>
         <div class="top-nav-links">
+            <a href="<?= $this->Url->build('/messages') ?>">Messages</a>
+            <a href="<?= $this->Url->build('/params') ?>">Params</a>
             <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
             <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
         </div>
